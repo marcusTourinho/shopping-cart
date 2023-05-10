bfc4c1f420bb3d375dbee2cd5c706c129b05ee61
Marcus Vinicius de Jesus Tourinho <viniciustourinho01@gmail.com>
2023-05-10 17:47:55 -0300

    Todos os Requisitos

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,35 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async (cep) => {
+  const BR_URL = 'https://brasilapi.com.br/api/cep/v2/';
+  const AWS_URL = 'https://cep.awesomeapi.com.br/json/';
+
+  try {
+    const cepArr = [
+      fetch(`${BR_URL}${cep}`),
+      fetch(`${AWS_URL}${cep}`),
+    ];
+
+    const response = await Promise.any(cepArr);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  const cepInput = document.querySelector('.cep-input').value;
+  const cepAddress = document.querySelector('.cart__address');
+  const cep = await getAddress(cepInput);
+
+  const promise1 = `${cep.address} - ${cep.district} - ${cep.city} - ${cep.state}`;
+  const promise2 = `${cep.street} - ${cep.neighborhood} - ${cep.city} - ${cep.state}`;
+
+  if (cep.address) {
+    cepAddress.innerHTML = promise1;
+  } else
+  if (cep.street) {
+    cepAddress.innerHTML = promise2;
+  } else {
+    cepAddress.innerHTML = 'CEP não encontrado';
+  }
 };
