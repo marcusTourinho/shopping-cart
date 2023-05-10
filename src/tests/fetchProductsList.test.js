62c1e302f0ead66a63ad298a394ce5895b4a89bb
Marcus Vinicius de Jesus Tourinho <viniciustourinho01@gmail.com>
2023-05-09 21:24:30 -0300

    Requisito 1

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,25 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProductsList('computer');
+    expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProductsList('computer');
+    expect(fetch).toHaveBeenCalledWith();
   });
 
-  // it('...', () => {
-  // });
+  it('retorna uma estrutura de dados igual ao objeto computadorSearch ao executar fetchProductsList', async () => {
+    const data = await fetchProductsList('computer');
+    await expect(data).toEqual(computadorSearch);
+  });
+
+  it('retorna erro com mensagem "Termo de busca não informado" ao executar fetchProductsList sem argumento', async () => {
+    await expect(fetchProductsList()).rejects.toThrow(new Error('Termo de busca não informado'));
+  });
 });
